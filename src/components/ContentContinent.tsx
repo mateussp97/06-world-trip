58adba5ffb3743260bf981e0c6824bb6a6e9f9f5
Mateus de Souza <mateusdesouzapaula97@gmail.com>
2021-04-09 00:58:29 -0300

    upgrade: setting responsive ContentContinent component

--- src/components/ContentContinent.tsx
+++ src/components/ContentContinent.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
   Flex,
+  HStack,
   Text,
   Box,
   Heading,
@@ -12,6 +13,7 @@ import {
   PopoverCloseButton,
   PopoverBody,
   Popover,
+  useMediaQuery,
 } from '@chakra-ui/react';
 import { RiQuestionLine } from 'react-icons/ri';
 
@@ -32,33 +34,46 @@ export default function ContentContinent({
   facts,
   summary,
 }: IContentContinent) {
+  const [isLargerThan1200] = useMediaQuery('(min-width: 1200px)');
+
   return (
-    <Flex w="100vw" h="100%">
+    <Flex w="100vw" h="100%" px="1rem">
       <Flex
         h="100%"
-        maxW="1160px"
         w="100%"
+        maxW="1160px"
         mx="auto"
         py="4rem"
+        direction={isLargerThan1200 ? 'row' : 'column'}
         alignItems="center"
-        justifyContent="space-between"
+        justifyContent={isLargerThan1200 ? 'space-between' : 'center'}
       >
         <Text
           lineHeight="9"
           letterSpacing="0.4px"
-          w="55%"
+          w={isLargerThan1200 ? '55%' : '100%'}
           color="gray.700"
           fontWeight="400"
-          fontSize="1rem"
+          fontSize={{ base: '1rem', md: '1.125rem', lg: '1.125rem' }}
         >
           {summary}
         </Text>
-        <Flex w="35%" alignItems="center" justifyContent="space-between">
+        <HStack
+          mt={isLargerThan1200 ? '0' : '2rem'}
+          w={isLargerThan1200 ? '35%' : '100%'}
+          alignItems="center"
+          justifyContent={isLargerThan1200 ? 'space-between' : 'center'}
+          spacing={{ base: '1.5rem', md: '1.5rem', lg: '2rem' }}
+        >
           <Box textAlign="center">
             <Heading color="yellow.400" fontWeight="600" fontSize="2rem">
               {countries}
             </Heading>
-            <Text color="gray.700" fontWeight="600" fontSize="1.25rem">
+            <Text
+              color="gray.700"
+              fontWeight="600"
+              fontSize={{ base: '1rem', md: '1.125rem', lg: '1.25rem' }}
+            >
               countries
             </Text>
           </Box>
@@ -66,7 +81,11 @@ export default function ContentContinent({
             <Heading color="yellow.400" fontWeight="600" fontSize="2rem">
               {language}
             </Heading>
-            <Text color="gray.700" fontWeight="600" fontSize="1.25rem">
+            <Text
+              color="gray.700"
+              fontWeight="600"
+              fontSize={{ base: '1rem', md: '1.125rem', lg: '1.25rem' }}
+            >
               languages
             </Text>
           </Box>
@@ -78,29 +97,39 @@ export default function ContentContinent({
               position="relative"
               color="gray.700"
               fontWeight="600"
-              fontSize="1.25rem"
+              fontSize={{ base: '1rem', md: '1.125rem', lg: '1.25rem' }}
             >
               cities +100
               <Popover closeOnBlur closeOnEsc flip isLazy>
                 <PopoverTrigger>
-                  <Icon ml="4px" as={RiQuestionLine} />
+                  <Icon
+                    cursor="pointer"
+                    _hover={{ color: 'yellow.400' }}
+                    transition="color 0.2s"
+                    ml="4px"
+                    as={RiQuestionLine}
+                  />
                 </PopoverTrigger>
                 <PopoverContent
                   position="absolute"
-                  bg="yellow.400"
+                  bg="gray.100"
                   top="3rem"
                   right="0"
                   color="gray.100"
                 >
-                  <PopoverHeader fontWeight="semibold">
-                    Popover placement
+                  <PopoverHeader
+                    fontSize={{ base: '0.875rem', md: '1rem', lg: '1rem' }}
+                    fontWeight="600"
+                    color="gray.700"
+                  >
+                    Facts about {title} continent
                   </PopoverHeader>
                   <PopoverArrow />
-                  <PopoverCloseButton bg="black" color="white" />
+                  <PopoverCloseButton bg="#e5e5e5" color="gray.700" />
                   <PopoverBody
-                    fontSize="1rem"
+                    fontSize={{ base: '0.875rem', md: '1rem', lg: '1rem' }}
                     fontWeight="400"
-                    color="gray.100"
+                    color="gray.700"
                   >
                     {facts}
                   </PopoverBody>
@@ -108,7 +137,7 @@ export default function ContentContinent({
               </Popover>
             </Text>
           </Box>
-        </Flex>
+        </HStack>
       </Flex>
     </Flex>
   );
