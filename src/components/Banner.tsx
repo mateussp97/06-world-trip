953e165749e3000d9b5157073d3f730ced1c4731
Mateus de Souza <mateusdesouzapaula97@gmail.com>
2021-04-09 00:02:22 -0300

    upgrade: setting responsive Banner component

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import { Flex, Text, Heading, Image } from '@chakra-ui/react';
+import { Flex, Text, Heading, Image, useMediaQuery } from '@chakra-ui/react';
 
 export default function Banner() {
+  const [isLargerThan1000] = useMediaQuery('(min-width: 1000px)');
+
   return (
     <Flex
       w="100vw"
-      h="335px"
+      h={isLargerThan1000 ? '335px' : '435px'}
       bgImage="url(/assets/background.png)"
       bgPosition="center"
       bgSize="cover"
+      px="1rem"
     >
       <Flex
         h="100%"
@@ -16,8 +19,9 @@ export default function Banner() {
         w="100%"
         mx="auto"
         display="flex"
+        direction={isLargerThan1000 ? 'row' : 'column'}
         alignItems="center"
-        justifyContent="space-between"
+        justifyContent={isLargerThan1000 ? 'space-between' : 'center'}
       >
         <Flex
           maxW="600px"
@@ -26,10 +30,18 @@ export default function Banner() {
           align="start"
           justify="space-between"
         >
-          <Heading color="gray.100" fontWeight="500" fontSize="2.25rem">
+          <Heading
+            color="gray.100"
+            fontWeight="500"
+            fontSize={{ base: '1.5rem', md: '1.75rem', lg: '2rem' }}
+          >
             7 Continents, infinite possibilities.
           </Heading>
-          <Text mt="5" color="gray.300" fontSize="1.25rem">
+          <Text
+            mt="5"
+            color="gray.300"
+            fontSize={{ base: '1rem', md: '1.125rem', lg: '1.25rem' }}
+          >
             The time has come to take the trip you have always dreamed of off
             the drawing board.
           </Text>
